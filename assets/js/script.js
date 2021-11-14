1c010974cacd8f3606faa8921ca8b6f7894215d0
Jordan Ophoff <noreply>
2021-11-13 23:34:03 -0500

    polished styling added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,40 +24,77 @@ let search5 = "";
 let search6 = "";
 let search7 = "";
 
-// pull from locat storage
-// $("#recentSearch0").html(localStorage.getItem("search0"));
-// $("#recentSearch0").html(localStorage.getItem("search1"));
-// $("#recentSearch0").html(localStorage.getItem("search2"));
-// $("#recentSearch0").html(localStorage.getItem("search3"));
-// $("#recentSearch0").html(localStorage.getItem("search4"));
-// $("#recentSearch0").html(localStorage.getItem("search5"));
-// $("#recentSearch0").html(localStorage.getItem("search6"));
-// $("#recentSearch0").html(localStorage.getItem("search7"));
 
 $(".form").on("keyup", ".form-control", function (e) {
-    if ("Enter" === e.originalEvent.code)
-    city = $("#cityInput").val();
-   
-    getWeatherByCity(city);
-    
+    e.preventDefault();
+    if ("Enter" === e.originalEvent.code) {
+        let city = $("#cityInput").val();
+
+        console.log(e.originalEvent.code);
+
+        let weatherHistory = JSON.parse(localStorage.getItem("list"))
+
+    if (weatherHistory === null) {
+        weatherHistory = []
+    }
+
+    if (weatherHistory.length === 8) {
+        weatherHistory.shift()
+    }
+    weatherHistory.push(city)
+
+    let history = $("<div class='btn btn-secondary mb-2'>")
+    history.text(city)
+
+    $("#prevSearch").prepend(history);
+
+    localStorage.setItem("list", JSON.stringify(weatherHistory))
+
+
+        getWeatherByCity(city);
+    };
 });
 
 $("#submitBtn").click(function () {
-    city = $("#cityInput").val();
+    // debugger;
+    let city = $("#cityInput").val();
+    let weatherHistory = JSON.parse(localStorage.getItem("list"))
 
-    
+    if (weatherHistory === null) {
+        weatherHistory = []
+    }
 
-    // localStorage.setItem(search7 = search6);
-    // localStorage.setItem(search6 = search5);
-    // localStorage.setItem(search5 = search4);
-    // localStorage.setItem(search4 = search3);
-    // localStorage.setItem(search3 = search2);
-    // localStorage.setItem(search2 = search1);
-    // localStorage.setItem(search1 = search0);
-    // localStorage.setItem(search0 = city);
+    if (weatherHistory.length === 8){
+        weatherHistory.shift()
+    }
+    weatherHistory.push(city)
 
+    let history = $("<div class='btn btn-secondary mb- 2'>")
+    history.text(city)
 
-    
+    $("#prevSearch").prepend(history);
+
+    localStorage.setItem("list", JSON.stringify(weatherHistory))
+
+    getWeatherByCity(city);
+})
+
+function showList() {
+// debugger;
+    let weatherHistory = JSON.parse(localStorage.getItem("list"))
+
+    if (weatherHistory !== null) {
+        for (let i = 0; i < weatherHistory.length; i++) {
+            let history = $("<div class='btn mb-2 btn-secondary'>")
+            history.text(weatherHistory[i])
+            $("#prevSearch").prepend(history);
+            
+        }
+    }
+}
+
+$("#prevSearch").click(function () {
+    let city = $(this).text();
     getWeatherByCity(city);
 });
 
@@ -106,45 +143,45 @@ let getWeatherByCoord = function (lat, lon) {
                 console.log(data);
 
                 uvi = data.current.uvi;
-                
+
                 // today
                 $("#currentDate").html(date0);
                 $("#img0").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png");
                 $("#currentCity").html(data.name);
                 $("#temp0").html(data.current.temp + "°F");
                 $("#wind0").html(data.current.wind_speed + " MPH");
-                $("#humidity0").html(data.current.humidity + " %");
+                $("#humidity0").html(data.current.humidity + "%");
                 $("#uvIndex").html(uvi);
                 // day one of five day
                 $("#first").html(date1);
                 $("#img1").attr("src", "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + ".png");
                 $("#temp1").html(data.daily[1].temp.day + "°F");
                 $("#wind1").html(data.daily[1].wind_speed + " MPH");
-                $("#humidity1").html(data.daily[1].humidity + " %");
+                $("#humidity1").html(data.daily[1].humidity + "%");
                 // day two of five day
                 $("#second").html(date2);
                 $("#img2").attr("src", "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + ".png");
                 $("#temp2").html(data.daily[2].temp.day + "°F");
                 $("#wind2").html(data.daily[2].wind_speed + " MPH");
-                $("#humidity2").html(data.daily[2].humidity + " %");
+                $("#humidity2").html(data.daily[2].humidity + "%");
                 // day three of five day
                 $("#third").html(date3);
                 $("#img3").attr("src", "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + ".png");
                 $("#temp3").html(data.daily[3].temp.day + "°F");
                 $("#wind3").html(data.daily[3].wind_speed + " MPH");
-                $("#humidity3").html(data.daily[3].humidity + " %");
+                $("#humidity3").html(data.daily[3].humidity + "%");
                 // day four of five day
                 $("#fourth").html(date4);
                 $("#img4").attr("src", "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + ".png");
                 $("#temp4").html(data.daily[4].temp.day + "°F");
                 $("#wind4").html(data.daily[4].wind_speed + " MPH");
-                $("#humidity4").html(data.daily[4].humidity + " %");
+                $("#humidity4").html(data.daily[4].humidity + "%");
                 //day five of five day
                 $("#fifth").html(date5);
                 $("#img5").attr("src", "http://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + ".png");
                 $("#temp5").html(data.daily[1].temp.day + "°F");
                 $("#wind5").html(data.daily[1].wind_speed + " MPH");
-                $("#humidity5").html(data.daily[1].humidity + " %");
+                $("#humidity5").html(data.daily[1].humidity + "%");
 
                 // uv Index
                 if (uvi > 2 && uvi <= 5) {
@@ -171,4 +208,4 @@ let getWeatherByCoord = function (lat, lon) {
 
 
 getWeatherByCity("detroit");
-
+showList();
